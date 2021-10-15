5bf15392456170a6ba7c100ff4ad17cd6af47799
ASnailman <harriskhoo21@gmail.com>
2021-10-15 00:28:18 -0500

    created teleop for mecanumdrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoalRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoalRobot.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -26,6 +27,7 @@ import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 import java.util.List;
 
+@Autonomous(name="UGRobot", group="MecanumDrive")
 public class UltimateGoalRobot extends LinearOpMode {
 
     static DcMotor frontleft;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VirtualRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VirtualRobot.java
@@ -15,6 +15,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
+@Autonomous(name="VirtualRobot", group="MecanumDrive")
 public class VirtualRobot extends LinearOpMode {
 
     static DcMotor BackLeft;
