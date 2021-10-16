8b46b6753f57127d2cb442c899e33092df169108
ASnailman <harriskhoo21@gmail.com>
2021-10-15 21:25:01 -0500

    created teleop for mecanumdrive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OfficialTeleop.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+//@TeleOp(name="MecanumDriveTeleop", group="MecanumDrive")
+public class OfficialTeleop extends LinearOpMode {
+
+    static DcMotor FrontLeft;
+    static DcMotor BackLeft;
+    static DcMotor FrontRight;
+    static DcMotor BackRight;
+    static MoveDirection Direction;
+
+    @Override
+    public void runOpMode() {
+
+        BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
+        BackRight = hardwareMap.get(DcMotor.class, "BackRight");
+        FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
+        FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+
+        SetDirection(MoveDirection.FORWARD);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double FLPower = (y + x + rx) / denominator;
+            double BLPower = (y - x + rx) / denominator;
+            double FRPower = (y - x - rx) / denominator;
+            double BRPower = (y + x - rx) / denominator;
+
+            //double FLPower = (y + x + rx);
+            //double BLPower = (y - x + rx);
+            //double FRPower = (y - x - rx);
+            //double BRPower = (y + x - rx);
+
+            FrontLeft.setPower(FLPower);
+            BackLeft.setPower(BLPower);
+            FrontRight.setPower(FRPower);
+            BackRight.setPower(BRPower);
+
+        }
+    }
+
+    private void SetDirection (MoveDirection direction) {
+
+        Direction = direction;
+
+        if (Direction == MoveDirection.FORWARD) {
+            FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+            FrontRight.setDirection(DcMotor.Direction.FORWARD);
+            BackLeft.setDirection(DcMotor.Direction.REVERSE);
+            BackRight.setDirection(DcMotor.Direction.FORWARD);
+        } else if (Direction == MoveDirection.REVERSE) {
+            FrontLeft.setDirection(DcMotor.Direction.FORWARD);
+            FrontRight.setDirection(DcMotor.Direction.REVERSE);
+            BackLeft.setDirection(DcMotor.Direction.FORWARD);
+            BackRight.setDirection(DcMotor.Direction.REVERSE);
+        }
+    }
+
+}
