aca6eb555bbd5a0089ce69c4cf5cef3ea3146ac2
ASnailman <harriskhoo21@gmail.com>
2021-11-03 01:10:11 -0500

    added more controls to attachment tuning class, added comments to organize

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AttachmentTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AttachmentTuning.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="AttachmentTuning", group="MecanumDrive")
 public class AttachmentTuning extends LinearOpMode {
@@ -13,15 +14,26 @@ public class AttachmentTuning extends LinearOpMode {
     static DcMotor BackLeft;
     static DcMotor FrontRight;
     static DcMotor BackRight;
+    static DcMotor Arm;
+    static DcMotor Rail;
     static MoveDirection Direction;
     static DcMotor Intake;
     static CRServo CarouselServo;
-    double intake_speed = 0.5;
+    static Servo BucketServo;
+    double intake_power = 0.5;
+    double arm_power = 0.5;
+    double rail_power = 0.5;
 
     boolean button_a_already_pressed = false;
     boolean button_b_already_pressed = false;
+    boolean button_x_already_pressed = false;
+    boolean button_y_already_pressed = false;
+    boolean button_x_already_pressed2 = false;
+    boolean button_y_already_pressed2 = false;
     boolean button_bumper_left_already_pressed = false;
     boolean button_bumper_right_already_pressed = false;
+    boolean button_bumper_left_already_pressed2 = false;
+    boolean button_bumper_right_already_pressed2 = false;
 
     @Override
     public void runOpMode() {
@@ -31,18 +43,25 @@ public class AttachmentTuning extends LinearOpMode {
         FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
         FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         Intake = hardwareMap.get(DcMotor.class, "Intake");
+        Arm = hardwareMap.get(DcMotor.class, "Arm");
+        Rail = hardwareMap.get(DcMotor.class, "Rail");
         CarouselServo = hardwareMap.get(CRServo.class, "carouselservo");
+        BucketServo = hardwareMap.get(Servo.class, "BucketServo");
 
-        CarouselServo.setDirection(DcMotorSimple.Direction.FORWARD);
+        AttachmentSetDirection();
         SetDirection(MoveDirection.FORWARD);
 
         waitForStart();
 
         while (opModeIsActive()) {
 
+            /****************************************
+             Increase Intake Power (G1): right bumper = power + 0.01, left bumper = power - 0.01
+             ***************************************/
+
             if (button_bumper_right_already_pressed == false) {
                 if (gamepad1.right_bumper) {
-                    intake_speed = intake_speed + 0.01;
+                    intake_power = intake_power + 0.01;
                     button_bumper_right_already_pressed = true;
                 }
             } else {
@@ -53,7 +72,7 @@ public class AttachmentTuning extends LinearOpMode {
 
             if (button_bumper_left_already_pressed == false) {
                 if (gamepad1.left_bumper) {
-                    intake_speed = intake_speed - 0.01;
+                    intake_power = intake_power - 0.01;
                     button_bumper_left_already_pressed = true;
                 }
             } else {
@@ -62,22 +81,105 @@ public class AttachmentTuning extends LinearOpMode {
                 }
             }
 
-            if (gamepad1.dpad_up) {
-                IntakeRunner(intake_speed);
-            } //runs intake with speed based on adjustments from pressing bumpers
+            /****************************************
+             Run Intake (G1): dpad_up = power, dpad_down = 0
+             ***************************************/
 
+            if (gamepad1.dpad_up) {
+                Intake.setPower(intake_power);
+            }
             if (gamepad1.dpad_down) {
-                IntakeRunner(0);
-            } //sets the intake power to 0, stops motor
+                Intake.setPower(0);
+            }
+
+            /****************************************
+             Run CarouselServo (G1): dpad_right = power 100%, dpad_down = power 0%
+             ***************************************/
 
             if (gamepad1.dpad_right) {
                 CarouselServo.setPower(1);
-            } //sets power 1, forwards
+            }
             if (gamepad1.dpad_left) {
                 CarouselServo.setPower(0);
-            } //sets power 0, still
+            }
 
-            telemetry.addData("Drive Speed", intake_speed);
+            /****************************************
+             Increase Arm Power (G2): right bumper = power + 0.01, left bumper = power - 0.01
+             ***************************************/
+
+            if (button_bumper_right_already_pressed2 == false) {
+                if (gamepad2.right_bumper) {
+                    arm_power = arm_power + 0.01;
+                    button_bumper_right_already_pressed2 = true;
+                }
+            } else {
+                if (!gamepad2.right_bumper) {
+                    button_bumper_right_already_pressed2 = false;
+                }
+            }
+
+            if (button_bumper_left_already_pressed2 == false) {
+                if (gamepad2.left_bumper) {
+                    arm_power = arm_power - 0.01;
+                    button_bumper_left_already_pressed2 = true;
+                }
+            } else {
+                if (!gamepad2.left_bumper) {
+                    button_bumper_left_already_pressed2 = false;
+                }
+            }
+
+            /****************************************
+             Run Arm (G2): dpad_up = power, dpad_down = 0
+             ***************************************/
+
+            if (gamepad2.dpad_up) {
+                Arm.setPower(arm_power);
+            }
+            if (gamepad2.dpad_down) {
+                Arm.setPower(0);
+            }
+
+            /****************************************
+             Increase Rail Power (G2): Button X = power + 0.01, Button Y = power - 0.01
+             ***************************************/
+
+            if (button_x_already_pressed2 == false) {
+                if (gamepad2.x) {
+                    rail_power = rail_power + 0.01;
+                    button_x_already_pressed2 = true;
+                }
+            } else {
+                if (!gamepad2.x) {
+                    button_x_already_pressed2 = false;
+                }
+            }
+
+            if (button_y_already_pressed2 == false) {
+                if (gamepad2.y) {
+                    rail_power = rail_power - 0.01;
+                    button_y_already_pressed2 = true;
+                }
+            } else {
+                if (!gamepad2.y) {
+                    button_y_already_pressed2 = false;
+                }
+            }
+
+            /****************************************
+             Run Rail (G2): Button A = power, Button B = 0
+             ***************************************/
+
+            if (gamepad2.a) {
+                Rail.setPower(rail_power);
+            }
+            if (gamepad2.b) {
+                Rail.setPower(0);
+            }
+
+            telemetry.addData("Intake Power", intake_power);
+            telemetry.addData("Arm Power", arm_power);
+            telemetry.addData("Rail Power", rail_power);
             telemetry.update();
 
         }
@@ -100,9 +202,13 @@ public class AttachmentTuning extends LinearOpMode {
         }
     }
 
-    private void IntakeRunner (double power) {
+    private void AttachmentSetDirection () {
 
-        Intake.setPower(power);
+        CarouselServo.setDirection(DcMotor.Direction.FORWARD);
+        BucketServo.setDirection(Servo.Direction.FORWARD);
+        Intake.setDirection(DcMotor.Direction.FORWARD);
+        Arm.setDirection(DcMotor.Direction.FORWARD);
+        Rail.setDirection(DcMotor.Direction.FORWARD);
 
     }
 
